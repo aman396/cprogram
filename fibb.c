e02db9052e3720c888e20168e54c7aae9e068518
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-09-17 18:30:16 +0530

    Indented code

--- fibb.c
+++ fibb.c
@@ -1,16 +1,14 @@
 #include "stdio.h"
-int main()
-{
-int a=1,b=1,c,d,i,n;
-c=a+b;
-printf("Enter the no. of digits:");
-scanf("%d",&n);
 
-for(i=1;i<=n;i++) {
-	c=a+b;
-	b=a;
-	a=c;
-	printf("  %d  \n",c);
-}
 
+int main() {
+	int a=1,b=1,c,i,n;
+	printf("Enter the no. of digits:");
+	scanf("%d",&n);
+		for(i=1;i<=n;i++) {
+			c=a+b;
+			b=a;
+			a=c;
+			printf("  %d  \n",c);
+		}
 }
